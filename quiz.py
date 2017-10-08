8b6c8b881c2aced91a0014b483f7434c9888002d
Exostin <exostin@Outlook.com>
2017-10-08 21:19:36 +0200

    upgrade

--- quiz.py
+++ quiz.py
@@ -5,19 +5,17 @@
 # 3.Execute with: "./pycalc.py"
 # or do first step and then "phyton pycalc.py"
 import string
+import pickle
 
 print('Welcome to quiz about python!', '\n')
 
 score = 0
-attempts = 2
 answer = ('Enter your answer: ')
-again = ('Incorrect! Try again.''\n')
-no_attempts = ('Incorrect! You ran out of your attempts.''\n')
 
 name = input('Enter your name: ')
 
 
-def question(message, options, correct, attempts=attempts):
+def question(message, options, correct):
     # message - string
     # options - list
     # correct - int
@@ -27,40 +25,37 @@ def question(message, options, correct, attempts=attempts):
     print(message)
     print(' '.join('{}: {}'.format(letter, answer)
                    for letter, answer in zip(optionLetters, options)))
-    while attempts > 0:
-        response = input(answer)
-        if response == optionLetters[correct]:
-            return True
-            score + 1
-        else:
-            attempts -= 1
-            print(again)
-
-    print(no_attempts)
-    return False
+    response = input(answer)
+    if response == optionLetters[correct]:
+        return True, score + 1
+    else:
+        return False
 
 
-question1 = question('Question 1: Who created python?',
+question1 = question('\nQuestion 1: Who created python?',
                      ['None of Above', 'Guido van Rossum', 'Mark Zuckerberg', 'Aliens'], 1)
-question2 = question('Question 2: What is the lastest version of python?',
+question2 = question('\nQuestion 2: What is the lastest version of python?',
                      ['4', '9', '2', '3'], 3)
-question3 = question('Question 3: Where is creator of python from?',
+question3 = question('\nQuestion 3: Where is creator of python from?',
                      ['USA', 'Mars', 'Netherland', 'Poland'], 2)
-question4 = question('Question 4: What sites from above are written in python?',
+question4 = question('\nQuestion 4: What sites from above are written in python?',
                      ['YouTube', 'All of them', 'Google', 'Reddit'], 1)
-question5 = question('Question 5: ',
+question5 = question('\nQuestion 5: ',
                      ['true', '', '', ''], 0)
-question6 = question('Question 6: ',
+question6 = question('\nQuestion 6: ',
                      ['', 'true', '', ''], 1)
-question7 = question('Question 7: ',
+question7 = question('\nQuestion 7: ',
                      ['', '', 'true', ''], 2)
-question8 = question('Question 8: ',
+question8 = question('\nQuestion 8: ',
                      ['true', '', '', ''], 0)
-question9 = question('Question 9: ',
+question9 = question('\nQuestion 9: ',
                      ['true', '', '', ''], 0)
-question10 = question('Question 10: ',
+question10 = question('\nQuestion 10: ',
                       ['', '', '', 'true'], 3)
-question11 = question('Question 11: ',
+question11 = question('\nQuestion 11: ',
                       ['', '', 'true', ''], 2)
-question12 = question('Question 12: ',
+question12 = question('\nQuestion 12: ',
                       ['', '', '', 'true'], 3)
+
+data = {name: score}
+pickle.dump(data, open("data.p", "rb"))
